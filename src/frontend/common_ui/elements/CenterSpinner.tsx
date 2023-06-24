b62f13dd52c705169874b4b6e08765c3b2762156
prokosna <prokosna@gmail.com>
2023-06-24 15:41:32 +0900

    Put spinner at the center of screen

--- src/frontend/common_ui/elements/CenterSpinner.tsx
+++ src/frontend/common_ui/elements/CenterSpinner.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export function CenterSpinner() {
   return (
     <>
-      <Center w="100%" h="100%">
+      <Center w="100vw" h="100vh">
         <Spinner boxSize="24" thickness="6px" color="brand.500"></Spinner>
       </Center>
     </>
