6e490375a1e538a860fdbcb0880db853d40685e2
prokosna <prokosna@gmail.com>
2024-03-18 23:11:14 +0900

    chore: remove comments

--- packages/frontend/src/features/file_explore/index.ts
+++ packages/frontend/src/features/file_explore/index.ts
@@ -1,12 +1,2 @@
 // Components
 export { FileExplore } from "./components/FileExplore";
-
-// States
-// export {
-//     useFileExploreFoldersState,
-//     useSetSelectedFileExploreFolderState,
-//     useRefreshFileExploreFolders
-// } from "./states/folders"
-// export {
-//     useFileExploreVisibleSongsState
-// } from "./states/songs"
