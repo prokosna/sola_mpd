1f469c4b1da5858e87631d5070483f04e802a7d8
prokosna <prokosna@gmail.com>
2024-03-31 18:13:50 +0900

    fix: refresh play queue and playlists at page transition (#99)

--- packages/frontend/src/features/location/components/LocationObserver.tsx
+++ packages/frontend/src/features/location/components/LocationObserver.tsx
@@ -2,6 +2,12 @@ import { useSetAtom } from "jotai";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+import {
+  ROUTE_HOME_PLAYLIST,
+  ROUTE_HOME_PLAY_QUEUE,
+} from "../../../const/routes";
+import { useRefreshPlayQueueSongsState } from "../../play_queue";
+import { useRefreshPlaylistsState } from "../../playlist";
 import { useSetSelectedSongsState } from "../../song_table";
 import { pathnameAtom } from "../states/location";
 
@@ -9,13 +15,29 @@ export function LocationObserver() {
   const location = useLocation();
   const setPathname = useSetAtom(pathnameAtom);
   const setSelectedSongs = useSetSelectedSongsState();
+  const refreshPlaylistsState = useRefreshPlaylistsState();
+  const refreshPlayQueueSongsState = useRefreshPlayQueueSongsState();
 
   useEffect(() => {
     setPathname(location.pathname);
 
     // When user moves to a different page, selected songs should be reset.
     setSelectedSongs([]);
-  }, [location.pathname, setPathname, setSelectedSongs]);
+
+    if (location.pathname === ROUTE_HOME_PLAYLIST) {
+      refreshPlaylistsState();
+    }
+
+    if (location.pathname === ROUTE_HOME_PLAY_QUEUE) {
+      refreshPlayQueueSongsState();
+    }
+  }, [
+    location.pathname,
+    refreshPlayQueueSongsState,
+    refreshPlaylistsState,
+    setPathname,
+    setSelectedSongs,
+  ]);
 
   return undefined;
 }
