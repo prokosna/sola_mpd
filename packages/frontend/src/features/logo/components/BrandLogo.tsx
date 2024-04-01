d979ad41ea98a46c33d8df578fa9ae0de3c082b6
prokosna <prokosna@gmail.com>
2024-04-02 00:20:59 +0900

    feat: remove logo when device width is narrow

--- packages/frontend/src/features/logo/components/BrandLogo.tsx
+++ packages/frontend/src/features/logo/components/BrandLogo.tsx
@@ -1,6 +1,14 @@
 import { Box, Text } from "@chakra-ui/react";
 
+import { useUserDeviceType } from "../../user_device";
+
 export function BrandLogo() {
+  const userDeviceType = useUserDeviceType();
+
+  if (userDeviceType !== "large") {
+    return null;
+  }
+
   return (
     <>
       <Box pl={6} m={0}>
