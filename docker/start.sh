e32e137b1f592285ce2b9815cec2abd569de67c4
prokosna <prokosna@gmail.com>
2024-06-22 07:57:56 +0900

    feat: support host network mode (#148)

--- docker/start.sh
+++ docker/start.sh
@@ -3,9 +3,23 @@
 set -e
 
 PORT_OPT="3000"
-if [[ "$1" == "--port" ]]; then
-  PORT_OPT="$2"
-fi
+NETWORK_MODE="bridge"
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    --port)
+      PORT_OPT="$2"
+      shift 2
+      ;;
+    --host)
+      NETWORK_MODE="host"
+      shift
+      ;;
+    *)
+      echo "Unknown parameter passed: $1"
+      exit 1
+      ;;
+  esac
+done
 
 ROOT_DIR="$(cd "$(dirname "$0")/.." || exit; pwd -P)"
 source "$ROOT_DIR/docker/const.sh"
@@ -14,8 +28,12 @@ EXTRA_ARGS=(
   --name "${CONTAINER_NAME}"
   -v sola_db:/app/packages/backend/db
   -e "USER=$USER"
-  -p $PORT_OPT:3000
-  --add-host host.docker.internal:host-gateway
 )
+
+if [[ "$NETWORK_MODE" = "host" ]]; then
+  EXTRA_ARGS+=("--network" "host" "-e" "PORT=$PORT_OPT")
+else
+  EXTRA_ARGS+=("-p" "$PORT_OPT:3000" "-e" "PORT=$PORT_OPT" "--add-host" "host.docker.internal:host-gateway")
+fi
 
 docker run -id --rm "${EXTRA_ARGS[@]}" $IMAGE_NAME
