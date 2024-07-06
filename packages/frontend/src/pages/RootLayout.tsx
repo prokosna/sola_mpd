6b651da17b6cd01e148e8ed60f2dc3345a3fa9fb
prokosna <prokosna@gmail.com>
2024-07-06 21:09:17 +0900

    fix: add suspense to RootLayout

--- packages/frontend/src/pages/RootLayout.tsx
+++ packages/frontend/src/pages/RootLayout.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 
 import "allotment/dist/style.css";
@@ -6,13 +7,16 @@ import "ag-grid-community/styles/ag-grid.min.css";
 import "ag-grid-community/styles/ag-theme-alpine.min.css";
 import "../agGrid.css";
 import "../global.css";
+import { TopLoadingProgressBar } from "../features/loading";
 import { LocationObserver } from "../features/location";
 
 export function RootLayout() {
   return (
     <>
-      <Outlet />
-      <LocationObserver />
+      <Suspense fallback={<TopLoadingProgressBar />}>
+        <Outlet />
+        <LocationObserver />
+      </Suspense>
     </>
   );
 }
