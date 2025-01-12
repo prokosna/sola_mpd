152eb7cf5e981147e8fbe40eb740150c46e1ff7a
prokosna <prokosna@gmail.com>
2025-01-12 16:12:14 +0900

    chore: upgrade ag-grid to v33 with legacy theme

--- packages/frontend/src/pages/RootLayout.tsx
+++ packages/frontend/src/pages/RootLayout.tsx
@@ -8,12 +8,39 @@ import "ag-grid-community/styles/ag-grid.min.css";
 import "ag-grid-community/styles/ag-theme-alpine.min.css";
 import "../agGrid.css";
 import "../global.css";
+import {
+	ClientSideRowModelApiModule,
+	ClientSideRowModelModule,
+	ColumnApiModule,
+	ModuleRegistry,
+	RowApiModule,
+	RowDragModule,
+	RowSelectionModule,
+	RowStyleModule,
+	TooltipModule,
+	ValidationModule,
+	provideGlobalGridOptions,
+} from "ag-grid-community";
 import { TopLoadingProgressBar } from "../features/loading";
 import { LocationObserver } from "../features/location";
 import { useJotaiStore } from "../useJotaiStore";
 
 export function RootLayout() {
 	const store = useJotaiStore();
+
+	ModuleRegistry.registerModules([
+		RowDragModule,
+		TooltipModule,
+		RowSelectionModule,
+		ClientSideRowModelModule,
+		ClientSideRowModelApiModule,
+		ColumnApiModule,
+		RowApiModule,
+		RowStyleModule,
+		ValidationModule,
+	]);
+	// TODO: Migrate to new theme API
+	provideGlobalGridOptions({ theme: "legacy" });
 
 	return (
 		<>
