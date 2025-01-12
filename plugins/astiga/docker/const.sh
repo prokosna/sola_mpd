dcd6c3e2e55b23f41883c21ff0834ce983b94137
prokosna <prokosna@gmail.com>
2025-01-13 00:02:38 +0900

    remove docker scripts

--- plugins/astiga/docker/const.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-export CONTAINER_NAME="sola_mpd_astiga_plugin"
-export IMAGE_PREFIX="sola_mpd_astiga_plugin_prebuilt"
-export IMAGE_TAG="latest"
-export IMAGE_NAME="${IMAGE_PREFIX}:${IMAGE_TAG}"
--- plugins/astiga/docker/remove.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-set -e
-
-ROOT_DIR="$(cd "$(dirname "$0")/.." || exit; pwd -P)"
-source "$ROOT_DIR/docker/const.sh"
-
-docker stop $CONTAINER_NAME || true
-docker rm $CONTAINER_NAME || true
