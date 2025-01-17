50be31cafa622b231c546863a0657fe4a18c874d
prokosna <prokosna@gmail.com>
2025-01-18 01:55:53 +0900

    remove: prev files

--- packages/frontend/src/features/recently_added/states/recentlyAddedStateRepository.ts
+++
@@ -1,8 +0,0 @@
-import { atomWithDefault } from "jotai/utils";
-
-import type { RecentlyAddedStateRepository } from "../services/RecentlyAddedStateRepository";
-
-export const recentlyAddedStateRepositoryAtom =
-	atomWithDefault<RecentlyAddedStateRepository>(() => {
-		throw new Error("Not initialized. Should be setup DI in the provider.");
-	});
