fd6112d16c61321e5f74b5e9ab361cfe85eb860e
prokosna <prokosna@gmail.com>
2025-05-18 14:53:54 +0900

    update CenterSpinner

--- packages/frontend/src/features/loading/components/CenterSpinner.tsx
+++ packages/frontend/src/features/loading/components/CenterSpinner.tsx
@@ -16,7 +16,7 @@ export function CenterSpinner({ className }: { className?: string }) {
 	return (
 		<>
 			<Center w="100%" h="100%" className={className}>
-				<Spinner boxSize="24" thickness="6px" color="brand.500" />
+				<Spinner boxSize="24" borderWidth="6px" color="brand.500" />
 			</Center>
 		</>
 	);
