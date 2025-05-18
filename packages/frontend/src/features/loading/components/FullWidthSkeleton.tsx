f46ed769f09bf3040d287dfb358765f7c2b809a6
prokosna <prokosna@gmail.com>
2025-05-18 14:55:56 +0900

    update FullWidthSkeleton

--- packages/frontend/src/features/loading/components/FullWidthSkeleton.tsx
+++ packages/frontend/src/features/loading/components/FullWidthSkeleton.tsx
@@ -17,7 +17,7 @@ export function FullWidthSkeleton({ className }: { className?: string }) {
 	return (
 		<>
 			<Box w="100%" h="100%" p="6" className={className}>
-				<SkeletonText mt="4" noOfLines={6} spacing="4" skeletonHeight="2" />
+				<SkeletonText mt="4" noOfLines={6} gap="4" height="2" />
 			</Box>
 		</>
 	);
