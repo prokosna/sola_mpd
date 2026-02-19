7b3521989995ad6815a748c2ef026f01bbe18b5e
prokosna <prokosna@gmail.com>
2026-02-20 00:40:26 +0900

    feat: highlight and increase width when moving separator (#325)

--- packages/frontend/src/features/file_explore/components/FileExplore.tsx
+++ packages/frontend/src/features/file_explore/components/FileExplore.tsx
@@ -1,6 +1,11 @@
 import { Box } from "@mantine/core";
 import clsx from "clsx";
-import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import {
+	Panel,
+	Group as PanelGroup,
+	Separator,
+	useDefaultLayout,
+} from "react-resizable-panels";
 import styles from "../../../ResizeHandle.module.css";
 import { FileExploreContent } from "./FileExploreContent";
 import { FileExploreNavigation } from "./FileExploreNavigation";
@@ -24,14 +29,23 @@ import { FileExploreNavigation } from "./FileExploreNavigation";
  * @returns Rendered file explorer component
  */
 export function FileExplore() {
+	const { defaultLayout, onLayoutChanged } = useDefaultLayout({
+		id: "file-explore",
+		storage: globalThis.localStorage,
+	});
+
 	return (
 		<Box w="100%" h="100%">
-			<PanelGroup direction="horizontal" autoSaveId="file-explore">
-				<Panel defaultSize={30} minSize={10}>
+			<PanelGroup
+				orientation="horizontal"
+				defaultLayout={defaultLayout}
+				onLayoutChanged={onLayoutChanged}
+			>
+				<Panel defaultSize="30%" minSize="10%" id="file-explore-navigation">
 					<FileExploreNavigation />
 				</Panel>
-				<PanelResizeHandle className={clsx(styles.handle, styles.vertical)} />
-				<Panel minSize={30}>
+				<Separator className={clsx(styles.handle, styles.vertical)} />
+				<Panel minSize="10%" id="file-explore-content">
 					<FileExploreContent />
 				</Panel>
 			</PanelGroup>
--- packages/frontend/src/features/playlist/components/Playlist.tsx
+++ packages/frontend/src/features/playlist/components/Playlist.tsx
@@ -1,6 +1,11 @@
 import { Box } from "@mantine/core";
 import clsx from "clsx";
-import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import {
+	Panel,
+	Group as PanelGroup,
+	Separator,
+	useDefaultLayout,
+} from "react-resizable-panels";
 import styles from "../../../ResizeHandle.module.css";
 import { PlaylistContent } from "./PlaylistContent";
 import { PlaylistNavigation } from "./PlaylistNavigation";
@@ -14,14 +19,23 @@ import { PlaylistNavigation } from "./PlaylistNavigation";
  * @returns Playlist view component
  */
 export function Playlist() {
+	const { defaultLayout, onLayoutChanged } = useDefaultLayout({
+		id: "playlist",
+		storage: globalThis.localStorage,
+	});
+
 	return (
 		<Box w="100%" h="100%">
-			<PanelGroup direction="horizontal" autoSaveId="playlist">
-				<Panel defaultSize={30} minSize={10}>
+			<PanelGroup
+				orientation="horizontal"
+				defaultLayout={defaultLayout}
+				onLayoutChanged={onLayoutChanged}
+			>
+				<Panel defaultSize="30%" minSize="10%" id="playlist-navigation">
 					<PlaylistNavigation />
 				</Panel>
-				<PanelResizeHandle className={clsx(styles.handle, styles.vertical)} />
-				<Panel minSize={30}>
+				<Separator className={clsx(styles.handle, styles.vertical)} />
+				<Panel minSize="10%" id="playlist-content">
 					<PlaylistContent />
 				</Panel>
 			</PanelGroup>
